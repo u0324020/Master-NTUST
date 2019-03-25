48bedbbeafe42496110abf5aad96346aa17043b5
u0324020 <gojane20@gmail.com>
2019-03-25 15:51:03 +0800

    Tweepy

--- Tweepy_Crawer/Tweepy_Test1.py
+++ Tweepy_Crawer/Tweepy_Test1.py
@@ -1,5 +1,8 @@
+import csv #Import csv
 import tweepy 
 
+csvFile = open('result.csv', 'a')
+csvWriter = csv.writer(csvFile)
 key = '39z9Oe8nXcPPLqvfXOxdWuchs'
 secret ='5DyXE8BJMCEPz8yeyAhnXA6TpH2VVrTrAmyp20VnOznBnltfEu'
 token = '1106489441860804608-PImjAF4NwAyePVYrqBg8GejRnQhVx5'
@@ -9,3 +12,18 @@ auth = tweepy.OAuthHandler(key,secret)
 auth.set_access_token(token,token_secret)
 
 api = tweepy.API(auth)
+#api.update_status('Hello python')
+#public_tweets = api.home_timeline()#self page
+#public_tweets = api.user_timeline('LeoDiCaprio') #others page
+# for tweet in public_tweets:
+# 	print(tweet.text)
+S_user = []
+for tweet in tweepy.Cursor(api.search,q='NTUST').items(10):
+	S_user.append(tweet.user.screen_name)
+	print('Tweet by: @' + tweet.user.screen_name)
+for i in S_user:
+	public_tweets = api.user_timeline(str(i)) #others page
+	for tweets in public_tweets:
+		csvWriter.writerow([str(i), tweets.text.encode('utf-8')])
+		print(tweets.text)
+csvFile.close()
