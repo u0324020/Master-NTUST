aa2755e2e0d0f71bf616942244d82c4825e882aa
u0324020 <gojane20@gmail.com>
2019-03-26 14:54:40 +0800

    0326update

--- Flask-教材/HelloWorld.py
+++ Flask-教材/HelloWorld.py
@@ -3,8 +3,9 @@
 from flask import Flask # import flask 的套件
 app = Flask(__name__) # app是整個code的主角
 
-@app.route('/Jane') #根目錄 
+@app.route('/') #根目錄 
 def index(): # function name 
+
     return "Hello World!" #return值
 
 app.run(port=8080,debug=True) #指定port number 及 更改code saving後會重run
--- Tweepy_Crawer/Tweepy_Test1.py
+++ Tweepy_Crawer/Tweepy_Test1.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
 import csv #Import csv
 import tweepy 
+import time
 
 csvFile = open('result.csv', 'a')
 csvWriter = csv.writer(csvFile)
@@ -11,19 +13,29 @@ token_secret = 'gUfoibf4eikGirplTdvnGTdkIbQFJ8kno2SGKlDHfYoqW'
 auth = tweepy.OAuthHandler(key,secret)
 auth.set_access_token(token,token_secret)
 
-api = tweepy.API(auth)
+api = tweepy.API(auth,wait_on_rate_limit=True)
 #api.update_status('Hello python')
 #public_tweets = api.home_timeline()#self page
 #public_tweets = api.user_timeline('LeoDiCaprio') #others page
 # for tweet in public_tweets:
 # 	print(tweet.text)
 S_user = []
+#抓出與NTUST相關文章的帳號
 for tweet in tweepy.Cursor(api.search,q='NTUST').items(10):
 	S_user.append(tweet.user.screen_name)
 	print('Tweet by: @' + tweet.user.screen_name)
+#McDonalds帳號的Follower
+ids = []
+for page in tweepy.Cursor(api.followers_ids, screen_name="J_ARamsey").pages():
+	ids.extend(page)
+	print(page)
+	time.sleep(60)
+print(len(ids))
+
+#依帳號抓所有資訊存入CSV
 for i in S_user:
-	public_tweets = api.user_timeline(str(i)) #others page
+	public_tweets = api.user_timeline(str(i),page=1,count=200, full_text=True) #others page
 	for tweets in public_tweets:
-		csvWriter.writerow([str(i), tweets.text.encode('utf-8')])
-		print(tweets.text)
+		csvWriter.writerow([str(i), tweets.created_at, tweets.text.encode('utf-8')])
+		#print(tweets.text)
 csvFile.close()
