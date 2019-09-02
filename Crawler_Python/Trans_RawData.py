0f0dafd98e6a5c1e190e95ae41551744de0f2f3d
u0324020 <gojane20@gmail.com>
2019-09-02 19:00:22 +0800

    update

--- Crawler_Python/Trans_RawData.py
+++ Crawler_Python/Trans_RawData.py
@@ -1,7 +1,7 @@
 # coding by Jane -2019
 # encoding: utf-8
 # 把原始資料轉成RawData
-# using : python 8. get_urlscan_page.py <inputfile> <startRow>
+# using : python 8. get_urlscan_page.py <inputfile>
 import requests
 import time
 from io import BytesIO
@@ -19,14 +19,20 @@ import sys
 import whois
 import datetime
 
+# domain_feature, country_feature, city_feature, asn_feature, securePercentage_feature, IPv6Percentage_feature,
+# uniqCountries_feature, adBlocked_feature, page_size_KB_feature, IP_count_feature, domains_count_feature, server_count_feature,
+# hashes_count_feature, requests_count_feature, samename_domain, count_DNS, de_creation, de_update, de_expiration, de_once_update = ""
 
-def Add_Row(row, col, data):
-    if col == 1 and data == 0:
-        print("None")
+
+def Add_CSV(arr):
+    df = pd.DataFrame(data=[arr], index=None,
+                      columns=None, dtype=None, copy=False)
+    df.to_csv("C:/Users/Jane/Desktop/NTU/Scam/Data/" + str(sys.argv[2]) + ".csv", encoding="utf-8",
+              mode='a', index=0, sep=',', header=None)
 
 
 def Feature_country(data):
-    country_list = []  # 'US,EU,TR,RU,TW,BR,RO,IN,IT,HU'
+    country_list = []
     if data == "null":
         data = "0"
     else:
@@ -35,51 +41,54 @@ def Feature_country(data):
                 data = "1"
     if data != "1":
         data = "0"
-    print("country" + data)
+    return data
 
 
 def Network_based(ID, df):
-    ID = ID
-
+    ID = ID - 1
     DNS_list = []
     ASN_list = []
-    ####### Domain ######
-    # domain_feature = str(df.domain[ID]).lower()
-    # for i in DNS_list:
-    #     if i in domain_feature:
-    #         domain_feature = "1"
-    #     else:
-    #         domain_feature = "0"
-    # print(domain_feature)
-    ####### Country ######
-    #country_feature = str(df.country[ID])
-    #country_feature = Feature_country(country_feature)
-    ###### Top_City #######
-    # city_feature = str(df.city[ID])
-    # if city_feature != "0":
-    #     city_feature = "1"
-    # print("city = " + city_feature)
-    ########## ASN ###########
-    # asn_feature = str(df.asn[ID])
-    # if asn_feature != "0":
-    #     for i in ASN_list:
-    #         if i in asn_feature:
-    #             asn_feature = "1"
-    #             print("asn = " + asn_feature)
-    #############IP###############
-    securePercentage_feature = str(df.securePercentage[ID])
-    IPv6Percentage_feature = str(df.IPv6Percentage[ID])
-    uniqCountries_feature = str(df.uniqCountries[ID])
-    adBlocked_feature = str(df.adBlocked[ID])
-    page_size_KB_feature = str(df.page_size_KB[ID])
-    IP_count_feature = str(df.IP_count[ID])
-    domains_count_feature = str(df.domains_count[ID])
-    server_count_feature = str(df.server_count[ID])
-    hashes_count_feature = str(df.hashes_count[ID])
-    requests_count_feature = str(df.requests_count[ID])
+    ###### Domain ######
+    domain_feature = str(df.domain[ID]).lower()
+    for i in DNS_list:
+        if i in domain_feature:
+            domain_feature = "1"
+        else:
+            domain_feature = "0"
+    First_list[0] = domain_feature
+    ###### Country ######
+    country_feature = str(df.country[ID])
+    First_list[1] = Feature_country(country_feature)
+    ##### Top_City #######
+    city_feature = str(df.city[ID])
+    if city_feature != "0":
+        city_feature = "1"
+    First_list[2] = city_feature
+    ######### ASN ###########
+    asn_feature = str(df.asn[ID])
+    if asn_feature != "0":
+        for i in ASN_list:
+            if i in asn_feature:
+                asn_feature = "1"
+                First_list[3] = asn_feature
+    else:
+        First_list[3] = "0"
+    ############IP###############
+    First_list[4] = str(df.securePercentage[ID])
+    First_list[5] = str(df.IPv6Percentage[ID])
+    First_list[6] = str(df.uniqCountries[ID])
+    First_list[7] = str(df.adBlocked[ID])
+    First_list[8] = str(df.page_size_KB[ID])
+    First_list[9] = str(df.IP_count[ID])
+    First_list[10] = str(df.domains_count[ID])
+    First_list[11] = str(df.server_count[ID])
+    First_list[12] = str(df.hashes_count[ID])
+    First_list[13] = str(df.requests_count[ID])
 
 
-def URL_whois(URL):
+def URL_whois(ID, df):
+    URL = df.url
+    print(URL[ID])
     samename_domain = 0
     samename_email = 0
     now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
@@ -91,10 +100,9 @@ def URL_whois(URL):
         str(whois_detals['creation_date']), "%Y-%m-%d %H:%M:%S")
     expiration = datetime.datetime.strptime(
         str(whois_detals['expiration_date'][0]), "%Y-%m-%d %H:%M:%S")
-
     de_update = (now - update).total_seconds()
     de_creation = (now - creation).total_seconds()
-    de_onec_update = de_creation - de_update
+    de_once_update = de_creation - de_update
     de_expiration = (expiration - now).total_seconds()
     count_DNS = len(whois_detals['name_servers'])
     org = whois_detals['org'].lower().replace(
@@ -109,16 +117,15 @@ def URL_whois(URL):
             print("no_samename_domain")
     for i in email:
         if org in i.lower():
-            samename_email += 1
+            samename_domain += 1
         else:
             print("no_samename_email")
-    print(de_update)
-    print(de_creation)
-    print(de_expiration)
-    print(de_onec_update)
-    print(count_DNS)
-    print(samename_domain)
-    print(samename_email)
+    First_list[14] = samename_domain
+    First_list[15] = count_DNS
+    First_list[16] = de_creation
+    First_list[17] = de_update
+    First_list[18] = de_expiration
+    First_list[19] = de_once_update
 
 
 def Open_txt(now_ID):
@@ -131,53 +138,58 @@ def Open_txt(now_ID):
     for i in brands:
         if i.lower() in content:
             print(i)
-            Add_Row(now_ID, 2, 1)
+            First_list[21] = 1
             count_keywords = count_keywords + 1
         else:
-            Add_Row(now_ID, 2, 0)
+            First_list[21] = 0
 
     keywords = []
-    location = 0
+    location = 22
     for i in keywords:
         if i.lower() in content:
             print(i)
-            Add_Row(now_ID, location, 1)
+            First_list[location] = 1
             count_keywords = count_keywords + 1
         else:
-            Add_Row(now_ID, location, 0)
+            First_list[location] = 0
         location = location + 1
     print("###############Count = " + str(count_keywords))
-    Add_Row(now_ID, 21, str(count_keywords))
+    First_list[41] = count_keywords
     count_comments = 0
     count_comments = content.count("<!--", 0, len(content))  # html
     count_comments += content.count("/*", 0, len(content))  # js
     count_comments += content.count("//", 0, len(content))  # js_sigle
     print("!!!!!comment!!!" + str(count_comments))
-    Add_Row(now_ID, 22, count_comments)
+    First_list[42] = count_comments
     count_links = 0
     count_links = content.count("http", 0, len(content))
     print("link = " + str(count_links))
-    Add_Row(now_ID, 23, count_comments)
+    First_list[43] = count_links
 
 
 def Open_CSV(path):
     df = pd.read_csv(path, skipinitialspace=True)
     now_ID = 0
     for Page in df.Page:
-        if Page == 1:
-            Add_Row(now_ID, 1, 1)
-            # Open_txt(now_ID)
-            Network_based(now_ID, df)
-            # print("Page:" + str(now_ID))
-        else:
-            Add_Row(now_ID, 1, 0)
-            # print("No Page:" + str(now_ID))
-            # arr = "0"
         now_ID = now_ID + 1
+        Network_based(now_ID, df)
+        URL_whois(now_ID, df)
+        print("For Page = " + str(now_ID))
+        if str(Page) == "1":
+            First_list[20] = 1
+            Open_txt(now_ID)
+            print("Page:" + str(now_ID))
+        if str(Page) == "0":
+            for i in range(20, 43):
+                First_list[i] = 0
+            print("No Page:" + str(now_ID))
+        # print(Final_list[:now_ID][:23])
+        print(First_list[:43])
+        Add_CSV(First_list)
 
 
 if __name__ == '__main__':
     file_path = "C:/Users/Jane/Desktop/NTU/Scam/Data/" + \
         str(sys.argv[1]) + ".csv"
+    First_list = [0] * 45
     Open_CSV(file_path)
-    # Open_txt(6)
