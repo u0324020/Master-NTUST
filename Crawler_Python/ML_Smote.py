bedd3e568c5e4b24aa4d2e1653605b55df80ff90
Jane Chen <gojane20@gmail.com>
2019-09-17 16:42:32 +0800

    Add files via upload

--- Crawler_Python/ML_Smote.py
+++ Crawler_Python/ML_Smote.py
@@ -18,10 +18,12 @@ from sklearn.metrics import precision_score, recall_score
 from matplotlib import pyplot as plt
 from numpy import loadtxt
 from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import roc_curve, auc
 import itertools
 from sklearn.metrics import roc_auc_score
 import time
+from sklearn.svm import SVC
 
 
 def Smote_upsampling(train_X, train_y):
@@ -157,6 +159,158 @@ def Xgboost_10fold(X, y):
     plt.show()
 
 
+def Tree(X, y):
+    class_names = ['Malicious', 'Scam']
+    start = time.time()
+    # seed = 7
+    # test_size = 0.33
+    # X_train, X_test, y_train, y_test = train_test_split(
+    #     X, y, test_size=test_size, random_state=seed)
+    X_train = X[:5000, :]
+    y_train = y[:5000]
+    X_test = X[5000:, :]
+    y_test = y[5000:]
+    XG = DecisionTreeClassifier(criterion='gini', max_depth=10)
+    XG.fit(X_train, y_train)
+    print(XG)
+    y_pred = XG.predict(X_test)
+    predictions = [round(value) for value in y_pred]
+
+    accuracy = accuracy_score(y_test, predictions)
+    print("Accuracy: %.2f%%" % (accuracy * 100.0))
+    end = time.time()
+    print('TIME:', end - start)
+    print('Accuracy:', accuracy_score(y_test, predictions))
+    print('Precision:', precision_score(y_test, predictions))
+    print('Recall:', recall_score(y_test, predictions))
+    print('F1_score:', f1_score(y_test, predictions))
+    print('AUC_score:', roc_auc_score(y_test, predictions))
+    fpr, tpr, threshold = roc_curve(y_test, predictions)
+    roc_auc = auc(fpr, tpr)
+    plt.figure()
+    lw = 2
+    plt.plot(fpr, tpr, color='darkorange',
+             lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)  # 假正率为横坐标，真正率为纵坐标做曲线
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic example')
+    plt.legend(loc="lower right")
+    plt.show()
+    plt.savefig('Xgboost-Only-ROC.png', dpi=150)
+    cnf_matrix = confusion_matrix(y_test, predictions)
+    np.set_printoptions(precision=2)
+    # print(cnf_matrix)
+    # Plot non-normalized confusion matrix
+    plt.figure()
+    plot_confusion_matrix(cnf_matrix, classes=class_names, cmap=plt.cm.Oranges,
+                          title='Confusion matrix(Xgboost), without normalization')
+    plt.savefig('Xgboost-Only(381).png', dpi=150)
+    # Plot normalized confusion matrix
+    plt.figure()
+    plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True,
+                          title='Normalized confusion matrix(Xgboost)')
+    plt.savefig('Xgboost-Only-1(381).png', dpi=150)
+    plt.show()
+
+
+def Tree_10flod(X, y):
+    class_names = ['Malicious', 'Scam']
+    start = time.time()
+    clf = DecisionTreeClassifier(criterion='entropy', max_depth=10)
+    y_pred = cross_val_predict(clf, X, y, cv=10)
+    scores = cross_val_score(clf, X, y, cv=10, scoring='accuracy')
+    print('標準差:', np.std(scores))
+    print('10次', scores)
+    print('平均', scores.mean())
+    end = time.time()
+    print('TIME:', end - start)
+    print('Accuracy:', accuracy_score(y, y_pred))
+    print('Precision:', precision_score(y, y_pred))
+    print('Recall:', recall_score(y, y_pred))
+    print('F1_score:', f1_score(y, y_pred))
+    print('AUC_score:', roc_auc_score(y, y_pred))
+    fpr, tpr, threshold = roc_curve(y, y_pred)
+    roc_auc = auc(fpr, tpr)
+    plt.figure()
+    lw = 2
+    plt.figure(figsize=(10, 10))
+    plt.plot(fpr, tpr, color='darkorange',
+             lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)  # 假正率为横坐标，真正率为纵坐标做曲线
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic example')
+    plt.legend(loc="lower right")
+    plt.show()
+    plt.savefig('Tree-ROC.png', dpi=150)
+    cnf_matrix = confusion_matrix(y, y_pred)
+    np.set_printoptions(precision=2)
+    # print(cnf_matrix)
+    # Plot non-normalized confusion matrix
+    plt.figure()
+    plot_confusion_matrix(cnf_matrix, classes=class_names, cmap=plt.cm.Oranges,
+                          title='Confusion matrix(Tree), without normalization')
+    plt.savefig('Tree(381).png', dpi=150)
+    # Plot normalized confusion matrix
+    plt.figure()
+    plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True,
+                          title='Normalized confusion matrix(Tree)')
+    plt.savefig('Tree-1(381).png', dpi=150)
+    plt.show()
+
+
+def SVM_10flod(X, y):
+    class_names = ['Malicious', 'Scam']
+    start = time.time()
+    clf = SVC(C=1, cache_size=500)
+    y_pred = cross_val_predict(clf, X, y, cv=10)
+    scores = cross_val_score(clf, X, y, cv=10, scoring='accuracy')
+    print('標準差:', np.std(scores))
+    print('10次', scores)
+    print('平均', scores.mean())
+    end = time.time()
+    print('TIME:', end - start)
+    print('Accuracy:', accuracy_score(y, y_pred))
+    print('Precision:', precision_score(y, y_pred))
+    print('Recall:', recall_score(y, y_pred))
+    print('F1_score:', f1_score(y, y_pred))
+    print('AUC_score:', roc_auc_score(y, y_pred))
+    fpr, tpr, threshold = roc_curve(y, y_pred)
+    roc_auc = auc(fpr, tpr)
+    plt.figure()
+    lw = 2
+    plt.plot(fpr, tpr, color='darkorange',
+             lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)  # 假正率为横坐标，真正率为纵坐标做曲线
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic example')
+    plt.legend(loc="lower right")
+    plt.show()
+    plt.savefig('Tree-ROC.png', dpi=150)
+    cnf_matrix = confusion_matrix(y, y_pred)
+    np.set_printoptions(precision=2)
+    # print(cnf_matrix)
+    # Plot non-normalized confusion matrix
+    plt.figure()
+    plot_confusion_matrix(cnf_matrix, classes=class_names, cmap=plt.cm.Oranges,
+                          title='Confusion matrix(Tree), without normalization')
+    plt.savefig('Tree(381).png', dpi=150)
+    # Plot normalized confusion matrix
+    plt.figure()
+    plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True,
+                          title='Normalized confusion matrix(Tree)')
+    plt.savefig('Tree-1(381).png', dpi=150)
+    plt.show()
+
+
 def plot_confusion_matrix(cm, classes,
                           normalize=False,
                           title='Confusion matrix',
@@ -193,8 +347,11 @@ if __name__ == '__main__':
     np.random.shuffle(train)
     train_X = train[:, 0:43]
     train_y = train[:, 43]
-    Xgboost_10fold(train_X, train_y)
+    #Tree_10flod(train_X, train_y)
+    #Xgboost_10fold(train_X, train_y)
     # Smote
-    # X, y = Smote_upsampling(train_X, train_y)
+    X, y = Smote_upsampling(train_X, train_y)
+    SVM_10flod(X, y)
+    # Tree(X, y)
     # Xgboost(X, y)
     # Xgboost_10fold(X, y)
